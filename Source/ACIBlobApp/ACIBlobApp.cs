fe1d35f5ca81ed79b4541332b45721b788b49c6e
monojit18 <datta.monojit@gmail.com>
2021-10-07 08:00:31 +0530

    initial commit

---
+++ Source/ACIBlobApp/ACIBlobApp.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace ACIBlobApp
+{
+    public static class ACIBlobApp
+    {
+        [FunctionName("ACIBlobApp")]
+        public static async Task Run([BlobTrigger("aciimageblob/{name}")]
+                                        CloudBlockBlob cloudBlockBlob,
+                                        [Blob("aciimageblob/{name}", FileAccess.ReadWrite)]
+                                        byte[] blobContents,
+                                        [Queue("aciimagequeue")]
+                                        IAsyncCollector<CloudQueueMessage>
+                                        cloudQueueMessageCollector,
+                                        ILogger logger)
+        {
+
+
+            logger.LogInformation(cloudBlockBlob.Name);
+            var cloudQueueMessage = new CloudQueueMessage(cloudBlockBlob.Name);
+            await cloudQueueMessageCollector.AddAsync(cloudQueueMessage);
+
+        }
+    }
+}
